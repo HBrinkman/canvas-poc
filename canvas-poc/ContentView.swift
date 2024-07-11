15c02a4d1f608cd918b7d0af19041dd1b476081b
Henry Brinkman <HenryBrinkman@gmail.com>
2024-07-11 12:49:52 +0200

    Simpel opzetje eraser en testje selectie tool

--- canvas-poc/ContentView.swift
+++ canvas-poc/ContentView.swift
@@ -21,28 +21,77 @@ struct ContentView: View {
 struct Home: View {
     
     @State var canvas = PKCanvasView()
-    @State var canvas2 = PKCanvasView()
-    @State var canvas3 = PKCanvasView()
-
+    @State var isErasing = false
+    @State var selectedColor: Color = .black
+    @State var selectedTool: PKInkingTool.InkType = .pencil
+    
     var body: some View {
         
-        ZStack {
-            DrawingView(canvas: $canvas)
+        ZStack(alignment: .bottomTrailing) {
+            DrawingView(canvas: $canvas, isErasing: $isErasing, selectedTool: $selectedTool, selectedColor: $selectedColor)
                 .ignoresSafeArea()
-            DrawingView(canvas: $canvas2)
-                .frame(width: 300, height: 300)
-                .clipShape(
-                    Circle()
-                )
-                .overlay(
-                    Circle()
-                        .stroke(.red, lineWidth: 2)
-                )
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.brown)
+                    .cornerRadius(8)
+                VStack {
+                    Spacer()
+                    HStack {
+                        Button {
+                            print("Done")
+                        } label: {
+                            Image(systemName: "checkmark.square.fill")
+                        }
+                        Button {
+                            print("Take screenshot")
+                        } label: {
+                            Image(systemName: "camera.fill")
+                        }
+                    }
+                    Spacer()
+                    Button {
+                        // TODO show overlay
+                        print("Select brush")
+                        isErasing = false
+                        selectedTool = .watercolor
+                    } label: {
+                        Image(systemName: "pencil")
+                    }
+                    Spacer()
+                    Button {
+                        print("Select color")
+                    } label: {
+                        Image(systemName: "eyedropper.full")
+                    }
+                    Spacer()
+                    Button {
+                        isErasing = true
+                    } label: {
+                        Image(systemName: "eraser.fill")
+                    }
+                    Spacer()
+                    HStack {
+                        Button {
+                            print("Undo")
+                        } label: {
+                            Image(systemName: "arrow.uturn.backward.circle.fill")
+                        }
+                        Button {
+                            print("Redo")
+                        } label: {
+                            Image(systemName: "arrow.uturn.forward.circle.fill")
+                        }
+                    }
+                    Spacer()
+                }
+            }
+            .frame(
+                maxWidth: 200,
+                maxHeight: .infinity
+            )
+            .padding(.vertical, 20)
+
             
-            DrawingView(canvas: $canvas3)
-                .frame(width: 150, height: 150)
-                .border(.green)
-                .offset(x: 150, y: 150)
         }
     }
 }
@@ -50,16 +99,22 @@ struct Home: View {
 struct DrawingView: UIViewRepresentable {
     
     @Binding var canvas: PKCanvasView
+    @Binding var isErasing: Bool
+    @Binding var selectedTool: PKInkingTool.InkType
+    @Binding var selectedColor: Color
+    
+    var ink: PKInkingTool {
+        PKInkingTool(selectedTool, color: UIColor(selectedColor), width: 20)
+    }
+    let eraser = PKEraserTool(.bitmap, width: 20)
     
     func makeUIView(context: Context) -> PKCanvasView {
         canvas.drawingPolicy = .anyInput
-        // Fill tool strokes verwijderen en background color setten op een bepaalde shape
-        // Met een eraser meteen background weer op wit zetten
-        canvas.backgroundColor = .blue
+        canvas.tool = isErasing ? eraser : ink
         return canvas
     }
     
     func updateUIView(_ uiView: PKCanvasView, context: Context) {
-        
+        uiView.tool = isErasing ? eraser : ink
     }
 }
